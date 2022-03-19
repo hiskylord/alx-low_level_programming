8ea3b95b28632b8241f78a3dd9ff55f5155bf4a5
ERUBURE DAVID EJIRO <55345141+hiskylord@users.noreply.github.com>
2022-03-19 03:52:14 +0100

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,13 +6,12 @@
  */
 void print_numbers(void)
 {
-  int i;
-
-  i = 0;
-  while (i < 10)
-    {
-      _putchar(i + '0');
-      i++;
-    }
-  _putchar('\n');
+int i;
+i = 0;
+while (i < 10)
+{
+_putchar(i + '0');
+i++;
+}
+_putchar('\n');
 }
