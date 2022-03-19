d9419365622113fd2a1a2c4c158335465c3d7805
ERUBURE DAVID EJIRO <55345141+hiskylord@users.noreply.github.com>
2022-03-19 03:44:02 +0100

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,10 +5,12 @@
  * Return: 1 if digit, else 0
  */
 int _isdigit(int c)
-{if (c > 47 && c < 58)
+{
+    if (c > 47 && c < 58)
     {
-    return (1); }
+    return (1);
+    }
  else{
-return (0); }
- 
+return (0); 
+ }
 }
