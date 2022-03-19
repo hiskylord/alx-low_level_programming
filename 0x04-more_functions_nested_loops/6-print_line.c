2509bc125689adb44c8e6533cbe28627af10cba4
ERUBURE DAVID EJIRO <55345141+hiskylord@users.noreply.github.com>
2022-03-19 03:58:12 +0100

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_line - draw a straight line
  * @n: number of times underscore is printed
@@ -7,14 +6,12 @@
  */
 void print_line(int n)
 {
-  int c;
-
-  c = 0;
-  while (c < n)
-    {
-      _putchar('_');
-
-      c++;
-    }
-  _putchar('\n');
+int c;
+c = 0;
+while (c < n)
+{
+_putchar('_');
+c++;
+}
+_putchar('\n');
 }
