edfbc946e24be8934220628bee8a4e88978c3617
ERUBURE DAVID EJIRO <55345141+hiskylord@users.noreply.github.com>
2022-03-22 05:54:15 +0100

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,36 +1,23 @@
-include "main.h"
+#include "main.h"
 /**
- * is_numerical - check if it is a digit
- * @n: Number
- * Return: If is a number, return 1 else return 0
- */
-int is_numerical(unsigned int n)
-{
-return (n >= '0' &&  n <= '9');
-}
-/**
- * _atoi - convert a string to an integer
- *@s: String
- * Return: Return the num
+ * _atoi - convert a string to an integer.
+ * @s: char type string
+ * Return: integer converted
  */
 int _atoi(char *s)
 {
-unsigned int number, i;
-int sign;
-sign = 1;
-number = 0;
-for (i = 0; s[i] != '\0'; i++)
+int sign = 1, resp = 0, fN;
+for (fN = 0; !(s[fN] >= 48 && s[fN] <= 57); fN++)
 {
-if (is_numerical(s[i]))
-{
-number = (s[i] - 48) + number * 10;
-if (s[i + 1] == ' ')
-break;
-}
-else if (s[i] == '-')
+if (s[fN] == '-')
 {
 sign *= -1;
 }
 }
-return (number *sign);
+for (int i = fN; s[i] >= 48 && s[i] <= 57; i++)
+{
+resp *= 10;
+resp += (s[i] - 48);
+}
+return (sign * resp);
 }
