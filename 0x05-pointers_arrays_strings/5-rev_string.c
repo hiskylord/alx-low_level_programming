ba598eae6302983dca9cc688fc3d23bf2a8326ce
ERUBURE DAVID EJIRO <55345141+hiskylord@users.noreply.github.com>
2022-03-22 05:10:03 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,40 +5,27 @@
  *
  *Return: void
  */
-
 void rev_string(char *s)
 {
-  char *start_c, *end_c, c;
-  int i, count;
-  int length = 0;
-
-  for (i = 0; s[i]; i++)
-    {
-      length++;
-    }
-
-  count = length;
-
-  start_c = s;
-  end_c = s;
-
-
-  for (i = 0; i < count - 1; i++)
-    {
-      end_c++;
-    }
-
-
-  for (i = 0; i < count / 2; i++)
-    {
-
-
-      c = *end_c;
-      *end_c = *start_c;
-      *start_c = c;
-
-
-      start_c++;
-      end_c--;
-    }
+char *start_c, *end_c, c;
+int i;
+int len = 0;
+for (i = 0; s[len]; i++)
+{
+len++;
+}
+start_c = s;
+end_c = s;
+for (i = 0; i < len - 1; i++)
+{
+end_c++;
+}
+for (i = 0; i < len / 2; i++)
+{
+c = *end_c;
+*end_c = *start_c;
+*start_c = c;
+start_c++;
+end_c--;
+}
 }
