250def544cb4acbcf25073a76f174f0b7c486a7a
ERUBURE DAVID EJIRO <55345141+hiskylord@users.noreply.github.com>
2022-03-22 04:36:23 +0100

    Update 4-main.c

--- 0x05-pointers_arrays_strings/4-main.c
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * main - check the code for Holberton School students.
  *
@@ -8,10 +7,9 @@
  */
 int main(void)
 {
-  char s[10] = "123456";
-
-  printf("%s\n", s);
-  rev_string(s);
-  printf("%s\n", s);
-  return (0);
+char s[10] = "123456";
+printf("%s\n", s);
+rev_string(s);
+printf("%s\n", s);
+return (0);
 }
