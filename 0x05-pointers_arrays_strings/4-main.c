0c4352e79649029d187645e7c43fef325d0a37f1
hiskylord <davidejiro49a@gmail.com>
2022-03-21 20:38:42 -0700

    putchar

--- 0x05-pointers_arrays_strings/4-main.c
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -1,17 +1,15 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-  char s[10] = "123456";
+  char *str;
 
-  printf("%s\n", s);
-  rev_string(s);
-  printf("%s\n", s);
+  str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+  print_rev(str);
   return (0);
 }
