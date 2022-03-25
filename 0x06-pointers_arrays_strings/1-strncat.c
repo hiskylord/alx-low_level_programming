73616d4e0eaa162e99aa4ede436aa943e98f8535
ERUBURE DAVID EJIRO <55345141+hiskylord@users.noreply.github.com>
2022-03-25 03:21:35 +0100

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,19 +6,14 @@
  * @n: number of bytes
  * Return: void
  */
-
 char *_strncat(char *dest, char *src, int n)
 {
-
-  int j, i;
-  for (j = 0; dest[j] != '\0'; j++)
-    ;
-
-  for (i = 0; i < n && src[i] != '\0'; i++)
-    dest[j + i] = src[i];
-
-  /*should end with a end of string char*/
-  dest[j + i] = '\0';
-
-  return (dest);
+int j, i;
+for (j = 0; dest[j] != '\0'; j++)
+;
+for (i = 0; i < n && src[i] != '\0'; i++)
+  dest[j + i] = src[i];
+/*should end with a end of string char*/
+dest[j + i] = '\0';
+return (dest);
 }
