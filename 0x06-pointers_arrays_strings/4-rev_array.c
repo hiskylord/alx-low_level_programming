94e0b1fff297f9ec8f2f85dc9683536926378f6e
ERUBURE DAVID EJIRO <55345141+hiskylord@users.noreply.github.com>
2022-03-25 03:32:39 +0100

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -5,15 +5,13 @@
  * @n: array length
  * Return: void
  */
-
 void reverse_array(int *a, int n)
 {
-  int i = 0, half;
-
-  for (half = n / 2; half > 0; half--, i++)
-    {
-      a[n - i - 1] += a[i];
-      a[i] = a[n - i - 1] - a[i];
-      a[n - i - 1] = a[n - i - 1] - a[i];
-    }
+int i = 0, half;
+for (half = n / 2; half > 0; half--, i++)
+{
+a[n - i - 1] += a[i];
+a[i] = a[n - i - 1] - a[i];
+a[n - i - 1] = a[n - i - 1] - a[i];
+}
 }
