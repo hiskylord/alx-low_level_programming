930e4f03f4e6574ad78c8fd17dffe1940169e177
ERUBURE DAVID EJIRO <55345141+hiskylord@users.noreply.github.com>
2022-03-25 03:34:01 +0100

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,14 +2,14 @@
 * @str: pointer
 * Return: capitalzied string
 */
-
 char *string_toupper(char *str)
 {
-  int i;
-  for (i = 0; str[i] != '\0'; i++)
-    {
-      if (str[i] <= 'z' && str[i] >= 'a')
-	str[i] -= 32;
-    }
-  return (str);
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] <= 'z' && str[i] >= 'a')
+{
+str[i] -= 32;
+}
+return (str);
 }
