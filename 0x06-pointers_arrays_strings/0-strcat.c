407ccbd4c2e6844815f362c8344481c1cf6b4094
ERUBURE DAVID EJIRO <55345141+hiskylord@users.noreply.github.com>
2022-03-25 03:12:45 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,18 +5,19 @@
  * @src: pointer source
  * Return: void
  */
-
 char *_strcat(char *s1, char *s2)
 {
-  int j = -1, i;
-  for (i = 0; s1[i] != '\0'; i++)
-    ;
-
-  do {
-    j++;
-    dest[i] = src[a];
-    i++;
-  } while (s2[j] != '\0');
-
-  return (s1);
+int j = -1, i;
+for (i = 0; s1[i] != '\0'; i++)
+;
+do 
+{
+j++;
+dest[i] = src[a];
+i++;
+} 
+while (s2[j] != '\0')
+{
+return (s1);
+}
 }
