f7c32eef25f6f8bb01c78f598ce203ca8d812f58
ERUBURE DAVID EJIRO <55345141+hiskylord@users.noreply.github.com>
2022-03-31 01:50:13 +0100

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,12 +6,12 @@
  */
 int _strlen_recursion(char *s)
 {
-  int c;
-  c = 0;
-  if (*s == '\0')
-    {
-      return (0);
-    }
-  c = 1 +  _strlen_recursion(s + 1);
-  return (c);
+int c;
+c = 0;
+if (*s == '\0')
+{
+return (0);
+}
+c = 1 +  _strlen_recursion(s + 1);
+return (c);
 }
