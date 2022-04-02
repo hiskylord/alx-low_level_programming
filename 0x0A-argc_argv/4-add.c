b38b93daefc643960855e4d2831b5847c90c9838
ERUBURE DAVID EJIRO <55345141+hiskylord@users.noreply.github.com>
2022-04-02 03:17:29 +0100

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * main - add 2 positive numbers and print the result
  * @argc: argument count
@@ -10,28 +9,28 @@
  * If one of the numbers contain non-digits, print Error.
  * Return: 1 if error, 0 if function runs properly.
  */
-
 int main(int argc, char *argv[])
 {
-  int total, i;
-  char *p;
-  int num;
-
-  total = 0;
-  if (argc > 1)
-    {
-      for (i = 1; argv[i]; i++)
-	{
-	  num = strtol(argv[i], &p, 10);
-	  if (!*p)
-	    total += num;
-	  else
-	    {
-	      printf("Error\n");
-	      return (1);
-	    }
-	}
-    }
-  printf("%d\n", total);
-  return (0);
+int total, i;
+char *p;
+int num;
+total = 0;
+if (argc > 1)
+{
+for (i = 1; argv[i]; i++)
+{
+num = strtol(argv[i], &p, 10);
+if(!*p)
+{
+total += num;
+}	  
+else
+{
+printf("Error\n");
+return (1);
+}
+}
+}
+printf("%d\n", total);
+return (0);
 }
